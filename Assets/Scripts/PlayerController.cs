44f2ae59528c84661b7416f73a775669051af984
dhruvduggal24 <dhduggal02@gmail.com>
2023-05-06 12:33:56 +0530

    Basic Player Movement Testing

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    [SerializeField] private float _speed = 1;
+    [SerializeField] private float _jumpForce = 200;
+    [SerializeField] private Rigidbody _rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vel = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _speed;
+        vel.y = _rb.velocity.y;
+        _rb.velocity = vel;
+
+        if (Input.GetKeyDown(KeyCode.Space)) _rb.AddForce(Vector3.up * _jumpForce);
+    }
+}
